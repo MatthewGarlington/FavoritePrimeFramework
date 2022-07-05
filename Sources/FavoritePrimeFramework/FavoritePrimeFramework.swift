acc4200aba5464559b27caa46d60433d898ce4c7
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-07-05 13:43:52 -0400

    Commit

--- Sources/FavoritePrimeFramework/FavoritePrimeFramework.swift
+++ Sources/FavoritePrimeFramework/FavoritePrimeFramework.swift
@@ -1,6 +1,38 @@
-public struct FavoritePrimeFramework {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import ComposableArchitecture
 
-    public init() {
+
+public enum FavoritesPrimeActions {
+    case deleteFavoritePrimes(IndexSet)
+}
+
+
+public func favoriteListReducer(state: inout [Int], action: FavoritesPrimeActions) {
+    switch action {
+    case let .deleteFavoritePrimes(indexSet):
+        for index in indexSet {
+            state.remove(at: index)
+        }
+    }
+}
+
+
+public struct FavoritePrimeView: View {
+    @ObservedObject var store: Store<[Int], FavoritesPrimeActions>
+    
+    public init(store: Store<[Int], FavoritesPrimeActions>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        List {
+            ForEach(store.value, id: \.self) { prime in
+                Text("\(prime)")
+            }
+            .onDelete { indexSet in
+                store.send(.deleteFavoritePrimes(indexSet))
+            }
+        }
+        .navigationTitle("Favorite Prime")
     }
 }
